d6fe0de639ec18c365f0c71a7c1cfc6cc28550b6
Leonid Shvab <matembit1996@gmail.com>
2021-11-16 03:35:21 +0200

    feat: video player

---
+++ src/components/vLoader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="Loader">
+    <div class="Loader__circle"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.Loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  &__circle {
+    border: 10px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 10px solid #000;
+    width: 120px;
+    height: 120px;
+    -webkit-animation: spin 2s linear infinite; /* Safari */
+    animation: spin 1.6s linear infinite;
+  }
+
+  /* Safari */
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
